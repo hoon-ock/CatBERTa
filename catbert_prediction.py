7efc2dda51ccd384cb55db4a8508d9a96f9055f1
hoon_ock <jock@andrew.cmu.edu>
2023-06-22 03:33:09 +0000

    prediction script update

--- catbert_prediction.py
+++ catbert_prediction.py
@@ -4,12 +4,12 @@ import pandas as pd
 from transformers import RobertaModel, RobertaTokenizerFast, RobertaConfig
 from tqdm import tqdm
 from model.dataset import FinetuneDataset, PretrainDataset
-from model.common import backbone_wrapper
+from model.common import backbone_wrapper, checkpoint_loader
 from torch.utils.data import DataLoader
 import os, yaml, pickle
 
 
-def ft_predict_fn(df, model, tokenizer, device):  
+def ft_predict_fn(df, model, tokenizer, device, emb_mode=False):  
     '''
     df: input dataframe with columns "text" and "target" (and "class lables")
     model: model to predict
@@ -34,40 +34,45 @@ def ft_predict_fn(df, model, tokenizer, device):
             ids = batch["ids"].to(device, dtype=torch.long)
             masks = batch["masks"].to(device, dtype=torch.long)
             targets = batch["target"].to(device, dtype=torch.float)
-            outputs = model(ids, masks).squeeze(-1) # Predictions from 1 batch of data.
+            if not emb_mode:
+                # energy prediction
+                outputs = model(ids, masks).squeeze(-1) 
+            else:
+                # embedding generation
+                outputs = model(ids, masks)['pooler_output']
             #import pdb; pdb.set_trace()
             outputs = outputs.detach().cpu().numpy()
             predictions.extend(outputs.tolist())
 
     return predictions
 
-# def get_energy_from_text(text, model, tokenizer, device):
-#     target = 0 
-#     text = np.array([text]).astype(object)
-#     data = FinetuneDataset(texts=text, targets=np.array([target]), 
-#                            tokenizer=tokenizer, 
-#                            seq_len= tokenizer.model_max_length-2)
-
-#     id = data[0]['ids'].unsqueeze(0)
-#     mask = data[0]['masks'].unsqueeze(0)
-    
-#     model.to(device)
-#     id = id.to(device)
-#     mask = mask.to(device)
-#     #import pdb; pdb.set_trace()
-#     model.eval()
-#     output = model(id, mask).squeeze(-1)
-#     return output.item()
 
 if __name__ == '__main__':
+    import argparse 
+    parser = argparse.ArgumentParser(description='Set the running mode')
+    parser.add_argument('--emb', action='store_true', help='Embedding generation mode')
+    args = parser.parse_args()
+    pred = 'energy'
+    if args.emb:
+        # embedding generation mode
+        pred = 'embed'
+        print('--------------- Embedding generation mode! ---------------')
+    else:
+        print('--------------- Energy prediction mode! ---------------')
+    
     # ================== 0. Load Checkpoint and Configs ==================
-    ckpt_dir = "./checkpoint/finetune/ft_0619_1527"
+    ckpt_dir = "/home/jovyan/CATBERT/checkpoint/pretrain/pt_0621_0442"
     # Load Training Config
-    ft_config = os.path.join(ckpt_dir, "ft_config.yaml")
-    paths = yaml.load(open(ft_config, "r"), Loader=yaml.FullLoader)['paths']
+    if 'pretrain' in ckpt_dir:
+        train_config = os.path.join(ckpt_dir, "pt_config.yaml")
+        pred = 'pt_' + pred
+    elif 'finetune' in ckpt_dir:
+        train_config = os.path.join(ckpt_dir, "ft_config.yaml")
+        pred = 'ft_' + pred
+    paths = yaml.load(open(train_config, "r"), Loader=yaml.FullLoader)['paths']
     val_data_path = paths["val_data"]
     tknz_path = paths["tknz"]
-    head = yaml.load(open(ft_config, "r"), Loader=yaml.FullLoader)['params']['model_head']
+    head = yaml.load(open(train_config, "r"), Loader=yaml.FullLoader)['params']['model_head']
     # Load Model Config
     model_config = yaml.load(open(os.path.join(ckpt_dir, "roberta_config.yaml"), "r"), Loader=yaml.FullLoader)
     roberta_config = RobertaConfig.from_dict(model_config)
@@ -75,40 +80,35 @@ if __name__ == '__main__':
     checkpoint = os.path.join(ckpt_dir, "checkpoint.pt")
     # Set Device 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    device = torch.device("cpu") 
-
+    device = torch.device("cpu") # for debugging
     # ================== 1. Load Model and Tokenizer ==================
     backbone = RobertaModel.from_pretrained('roberta-base', config=roberta_config, ignore_mismatched_sizes=True)
     model = backbone_wrapper(backbone, head)
     model.load_state_dict(torch.load(checkpoint))
-    
     max_len = model.roberta_model.embeddings.position_embeddings.num_embeddings
-    tokenizer = RobertaTokenizerFast.from_pretrained(tknz_path, max_len=max_len)                                
 
+    if args.emb:
+        # To generate embeddings, we need to load the model without the head.
+        # So, we need to load the model from the checkpoint only on the backbone.
+        model = checkpoint_loader(backbone, checkpoint, load_on_roberta=True)
+        max_len = model.embeddings.position_embeddings.num_embeddings
+
+    tokenizer = RobertaTokenizerFast.from_pretrained(tknz_path, max_len=max_len)                                
     # ================== 2. Load Data ==================                               
     df_val = pd.read_pickle(val_data_path)
-    # df_val = df_val.sample(5, random_state=42) # for debugging
-    # df_val.set_index('id', inplace=True)  
+    df_val = df_val.sample(500, random_state=42) # for debugging
     # ================== 3. Obtain Predictions ==================
-    predictions = ft_predict_fn(df_val, model, tokenizer, device)
+    predictions = ft_predict_fn(df_val, model, tokenizer, device, emb_mode=args.emb)
     
     # save predictions as dictionary with id as key
-    save_path = os.path.join(ckpt_dir, "predictions.pkl")
     results = {}
     for i in range(len(df_val)):
         results[df_val.iloc[i]['id']] = predictions[i]
+    
+    save_dir = "/home/jovyan/CATBERT/results/catbert"
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    save_path = os.path.join(save_dir, f"catbert_{pred}_{ckpt_dir.split('/')[-1]}.pkl")
     with open(save_path, "wb") as f:
-        pickle.dump(results, f)
-
-
-    #import pdb; pdb.set_trace()
-    # obtain catbert predictions
-    # results={}
-    # for i in tqdm(df_val.index):
-    #     results[i] = get_energy_from_text(df_val.loc[i, 'text'], model, tokenizer, device)
-    # save results
-    
-    import pdb; pdb.set_trace()
-    # with open(save_path, "wb") as f:
-    #     pickle.dump(results, f)
-    
+        pickle.dump(results, f)   
